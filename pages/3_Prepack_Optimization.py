5ef7425b9ecea35384d820d134688edc4991f0a1
thoriusapp <mhakan_ugur@yahoo.com>
2025-11-02 17:51:45 +0300

    Update 3_Prepack_Optimization.py

--- pages/3_Prepack_Optimization.py
+++ pages/3_Prepack_Optimization.py
@@ -49,7 +49,7 @@ st.sidebar.page_link("pages/0_Veri_Yukleme.py", label="📤 Veri Yükleme", icon
 st.sidebar.markdown("---")
 st.sidebar.page_link("pages/1_Lost_Sales.py", label="📉 Lost Sales", icon="📊")
 st.sidebar.page_link("pages/2_Sevkiyat.py", label="🚚 Sevkiyat Planlama", icon="📦")
-st.sidebar.page_link("pages/3_Prepack_Optimizasyonu.py", label="📦 Prepack Optimizasyon", icon="📦")
+st.sidebar.page_link("pages/3_Prepack_Optimization.py", label="📦 Prepack Optimizasyon", icon="📦")
 st.sidebar.page_link("pages/4_PO.py", label="💵 Alım Sipariş (PO)", icon="🛒")
 
 st.sidebar.markdown("---")
